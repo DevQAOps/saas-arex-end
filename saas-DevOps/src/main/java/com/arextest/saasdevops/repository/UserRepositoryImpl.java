d0762b688c9c268fdb69b6df63fc5d60b2234dd9
wildeslam <yushuwang@trip.com>
2024-04-19 16:26:57 +0800

    devops init

--- saas-DevOps/src/main/java/com/arextest/saasdevops/repository/UserRepositoryImpl.java
+++ saas-DevOps/src/main/java/com/arextest/saasdevops/repository/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.arextest.saasdevops.repository;
 import com.arextest.web.core.repository.mongo.util.MongoHelper;
 import com.arextest.web.model.dao.mongodb.UserCollection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -23,21 +24,21 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public boolean addUser(List<String> emails) {
+        BulkOperations bulkOps = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, UserCollection.class);
         for (String email : emails) {
             Query query = Query.query(Criteria.where(UserCollection.Fields.userName).is(email));
             Update update = MongoHelper.getUpdate();
             update.set(UserCollection.Fields.userName, email);
-            mongoTemplate.upsert(query, update, UserCollection.class);
+            bulkOps.upsert(query, update);
         }
+        bulkOps.execute();
         return true;
     }
 
     @Override
     public boolean removeUser(List<String> emails) {
-        for (String email : emails) {
-            Query query = Query.query(Criteria.where(UserCollection.Fields.userName).is(email));
-            mongoTemplate.remove(query, UserCollection.class);
-        }
+        Query query = Query.query(Criteria.where(UserCollection.Fields.userName).in(emails));
+        mongoTemplate.remove(query, UserCollection.class);
         return true;
     }
 }
