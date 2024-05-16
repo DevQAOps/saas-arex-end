0bf6cd864b89aebd15471569951f308aa3376f3c
wildeslam <yushuwang@trip.com>
2024-05-16 15:57:49 +0800

    feat: FinalizeSaasUser interface

--- saas-DevOps/src/main/java/com/arextest/saasdevops/controller/UserManageController.java
+++ saas-DevOps/src/main/java/com/arextest/saasdevops/controller/UserManageController.java
@@ -4,6 +4,7 @@ import com.arextest.common.model.response.Response;
 import com.arextest.common.utils.ResponseUtils;
 import com.arextest.common.utils.TenantContextUtil;
 import com.arextest.saasdevops.model.contract.AddUserRequest;
+import com.arextest.saasdevops.model.contract.FinalizeSaasUserRequest;
 import com.arextest.saasdevops.model.contract.InitSaasUserRequest;
 import com.arextest.saasdevops.model.contract.RemoveUserRequest;
 import com.arextest.saasdevops.service.UserManageService;
@@ -32,6 +33,13 @@ public class UserManageController {
   public Response initSaasUser(@RequestBody InitSaasUserRequest request) {
     TenantContextUtil.setTenantCode(request.getTenantCode());
     return ResponseUtils.successResponse(userManageService.initSaasUser(request));
+  }
+
+  @PostMapping("/finalizeSaasUser")
+  @ResponseBody
+  public Response finalizeSaasUser(@RequestBody FinalizeSaasUserRequest request) {
+    TenantContextUtil.setTenantCode(request.getTenantCode());
+    return ResponseUtils.successResponse(userManageService.finalizeSaasUser(request));
   }
 
   @PostMapping("/addUser")
---
+++ saas-DevOps/src/main/java/com/arextest/saasdevops/model/contract/FinalizeSaasUserRequest.java
@@ -0,0 +1,12 @@
+package com.arextest.saasdevops.model.contract;
+
+import lombok.Data;
+
+/**
+ * @author wildeslam.
+ * @create 2024/5/16 15:48
+ */
+@Data
+public class FinalizeSaasUserRequest extends BaseRequest {
+
+}
