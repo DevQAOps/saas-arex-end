649f89f42c500ab48edfbba1b73b545e66514dbb
wildeslam <yushuwang@trip.com>
2024-06-20 15:05:27 +0800

    feat:create collection&index when init Saas tenant

--- saas-DevOps/src/main/java/com/arextest/saasdevops/service/impl/UserManageServiceImpl.java
+++ saas-DevOps/src/main/java/com/arextest/saasdevops/service/impl/UserManageServiceImpl.java
@@ -1,10 +1,11 @@
 package com.arextest.saasdevops.service.impl;
 
-import com.arextest.common.saas.tenant.TenantRedisHandler;
 import com.arextest.common.saas.model.SaasSystemConfigurationKeySummary;
 import com.arextest.common.saas.model.dao.SaasSystemConfigurationCollection;
-import com.arextest.common.saas.model.dto.SaasSystemConfiguration;
+import com.arextest.common.saas.tenant.TenantRedisHandler;
 import com.arextest.config.model.dao.config.SystemConfigurationCollection;
+import com.arextest.model.mock.AREXMocker;
+import com.arextest.model.mock.MockCategoryType;
 import com.arextest.saasdevops.mapper.TenantStatusMapper;
 import com.arextest.saasdevops.model.contract.FinalizeSaasUserRequest;
 import com.arextest.saasdevops.model.contract.InitSaasUserRequest;
@@ -12,13 +13,28 @@ import com.arextest.saasdevops.model.dto.TenantStatusInfo;
 import com.arextest.saasdevops.repository.UserRepository;
 import com.arextest.saasdevops.service.TenantManageService;
 import com.arextest.saasdevops.service.UserManageService;
+import com.arextest.storage.enums.MongoCollectionIndexConfigEnum;
+import com.arextest.storage.enums.MongoCollectionIndexConfigEnum.FieldConfig;
+import com.arextest.storage.enums.MongoCollectionIndexConfigEnum.TtlIndexConfig;
+import com.arextest.storage.repository.ProviderNames;
 import com.mongodb.BasicDBObject;
+import com.mongodb.MongoCommandException;
+import com.mongodb.client.ListIndexesIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,12 +42,15 @@ import org.springframework.stereotype.Service;
  * @author wildeslam.
  * @create 2024/4/17 14:26
  */
+@Slf4j
 @Service
 public class UserManageServiceImpl implements UserManageService {
 
   private static final String COMPANY_DATABASE_FORMAT = "%s_arex_storage_db";
-
   private static final String MONGO_DATABASE_PASSWORD = "iLoveArex";
+  private static final String COLLECTION_SUFFIX = "Mocker";
+  private static final String EXPIRATION_TIME_COLUMN_NAME = "expirationTime";
+
 
   @Autowired
   private UserRepository userRepository;
@@ -57,6 +76,9 @@ public class UserManageServiceImpl implements UserManageService {
     MongoDatabase mongoDatabase = mongoClient.getDatabase(databaseName);
 
     createDatabaseAndUser(databaseName, mongoDatabase);
+
+    // create collection & index
+    createCollectionAndIndex(mongoDatabase);
 
     // insert jwt seed to system configuration
     insertJwtSeedToSystemConfiguration(tenantCode, currentTime, mongoDatabase);
@@ -107,6 +129,111 @@ public class UserManageServiceImpl implements UserManageService {
     BasicDBObject dropUserCommand = new BasicDBObject("dropUser", "arex");
     mongoDatabase.runCommand(dropUserCommand);
     mongoDatabase.drop();
+  }
+
+
+  private void createCollectionAndIndex(MongoDatabase mongoDatabase) {
+    for (MongoCollectionIndexConfigEnum indexConfigEnum : MongoCollectionIndexConfigEnum.values()) {
+      setIndexByEnum(indexConfigEnum, mongoDatabase);
+    }
+    ensureMockerQueryIndex(mongoDatabase);
+  }
+
+  private void setIndexByEnum(MongoCollectionIndexConfigEnum indexConfigEnum,
+      MongoDatabase mongoDatabase) {
+    MongoCollection<Document> collection = mongoDatabase.getCollection(
+        indexConfigEnum.getCollectionName());
+
+    ListIndexesIterable<Document> existedIndexes = collection.listIndexes();
+
+    List<Pair<Document, IndexOptions>> toAddIndexes = new ArrayList<>();
+
+    indexConfigEnum.getIndexConfigs().forEach(indexConfig -> {
+      List<FieldConfig> fieldConfigs = indexConfig.getFieldConfigs();
+      Document index = new Document();
+      for (FieldConfig fieldConfig : fieldConfigs) {
+        index.append(fieldConfig.getFieldName(),
+            fieldConfig.getAscending() != Boolean.FALSE ? 1 : -1);
+      }
+      IndexOptions indexOptions = new IndexOptions();
+      if (indexConfig.getUnique() != null) {
+        indexOptions.unique(indexConfig.getUnique());
+      }
+      if (indexConfig.getTtlIndexConfig() != null) {
+        TtlIndexConfig ttlIndexConfig = indexConfig.getTtlIndexConfig();
+        indexOptions.expireAfter(ttlIndexConfig.getExpireAfter(), ttlIndexConfig.getTimeUnit());
+      }
+      indexOptions.background(true);
+      toAddIndexes.add(Pair.of(index, indexOptions));
+    });
+
+    // add new indexes which not exist
+    for (Pair<Document, IndexOptions> newIndex : toAddIndexes) {
+      try {
+        collection.createIndex(newIndex.getLeft(), newIndex.getRight());
+      } catch (Exception e) {
+        LOGGER.error("Failed to create index: {}", newIndex.getLeft(), e);
+      }
+    }
+  }
+
+  private void ensureMockerQueryIndex(MongoDatabase database) {
+    for (MockCategoryType category : MockCategoryType.DEFAULTS) {
+      for (Field field : ProviderNames.class.getDeclaredFields()) {
+        String providerName = null;
+        try {
+          providerName = (String) field.get(ProviderNames.class);
+        } catch (IllegalAccessException e) {
+          LOGGER.error("get provider name failed", e);
+          continue;
+        }
+
+        MongoCollection<AREXMocker> collection =
+            database.getCollection(getCollectionName(category, providerName),
+                AREXMocker.class);
+        try {
+          Document index = new Document();
+          index.append(AREXMocker.Fields.recordId, 1);
+          collection.createIndex(index);
+        } catch (MongoCommandException e) {
+          LOGGER.info("create index failed for {}", category.getName(), e);
+        }
+
+        try {
+          Document index = new Document();
+          index.append(AREXMocker.Fields.appId, 1);
+          index.append(AREXMocker.Fields.operationName, 1);
+          collection.createIndex(index);
+        } catch (MongoCommandException e) {
+          LOGGER.info("create index failed for {}", category.getName(), e);
+        }
+
+        if (providerName.equals(ProviderNames.DEFAULT)) {
+          setTTLIndexInMockerCollection(category, database);
+        }
+      }
+    }
+  }
+
+  private String getCollectionName(MockCategoryType category, String providerName) {
+    return providerName + category.getName() + COLLECTION_SUFFIX;
+  }
+
+  private void setTTLIndexInMockerCollection(MockCategoryType category,
+      MongoDatabase mongoDatabase) {
+    String categoryName = getCollectionName(category, ProviderNames.DEFAULT);
+    MongoCollection<AREXMocker> collection = mongoDatabase.getCollection(categoryName,
+        AREXMocker.class);
+    Bson index = new Document(EXPIRATION_TIME_COLUMN_NAME, 1);
+    IndexOptions indexOptions = new IndexOptions().expireAfter(0L, TimeUnit.SECONDS);
+    indexOptions.background(true);
+    try {
+      collection.createIndex(index, indexOptions);
+    } catch (MongoCommandException e) {
+      // ignore
+      collection.dropIndex(index);
+      collection.createIndex(index, indexOptions);
+    }
   }
 
   private void insertJwtSeedToSystemConfiguration(String tenantCode, Long currentTime,
